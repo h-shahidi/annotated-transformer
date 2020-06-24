c81629bd0875cc0202d4a279fd152d5c391ad73c
Hamidreza <shahidi.hamidreza@gmail.com>
2020-06-23 23:14:44 -0400

    add criterion to train with real data

--- train_with_real_data.py
+++ train_with_real_data.py
@@ -1,6 +1,6 @@
 from utils.data_loading import load_data
 from utils.train import make_model
-
+from utils.label_smoothing import LabelSmoothing
 
 devices = [5, 6]
 
@@ -10,3 +10,5 @@ if __name__ == "__main__":
     pad_idx = TGT.vocab.stoi["<blank>"]
     model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
     model.cuda()
+    criterion = LabelSmoothing(vocab_size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
+    criterion.cuda()
